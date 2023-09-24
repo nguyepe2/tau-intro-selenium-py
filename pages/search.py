36a29b35d9331910edcee9d7cca6e484b1e44576
Peter Nguyen <32175436+nguyepe2@users.noreply.github.com>
2023-09-24 10:19:46 -0700

    Upload project
    My repository used for completing Test Automation University's "Selenium WebDriver with Python" course

---
+++ pages/search.py
@@ -0,0 +1,31 @@
+"""
+This module contains DuckDuckGoSearchPage, the page object for the DuckDuckGo search page
+"""
+
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+
+class DuckDuckGoSearchPage:
+
+    # URL
+
+    URL = 'https://www.duckduckgo.com'
+
+    # Locators
+
+    SEARCH_INPUT = (By.ID, 'searchbox_input')
+
+    # initializer
+
+    def __init__(self, browser):
+        self.browser = browser
+
+    # Interaction methods
+
+    def load(self):
+        self.browser.get(self.URL)
+
+    def search(self, phrase):
+        search_input = self.browser.find_element(*self.SEARCH_INPUT)
+        search_input.send_keys(phrase + Keys.RETURN)
